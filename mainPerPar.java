cee725def0875e66aeb134b6990be52187fc3186
4bysss <74374776+4bysss@users.noreply.github.com>
2022-11-16 20:32:39 +0100

    Add files via upload

---
+++ mainPerPar.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
+import javax.naming.InitialContext;
+
+public class mainPerPar {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        int principio = 1;
+        int finaleo = 1000;
+        int dif = finaleo - principio;
+        int cores = Runtime.getRuntime().availableProcessors();
+        int i = 0;
+        Integer result = 0;
+//77o7sd
+        ExecutorService cosa = Executors.newFixedThreadPool(cores);
+        List<Future<Integer>>futuro = new ArrayList<Future<Integer>>();
+        for(i = 0;i<dif/cores-1;i++){
+            futuro.add(cosa.submit(new numPerfectosPar((principio+(dif/cores)*i),(principio+(dif/cores)*(i+1)))));
+
+
+        }
+      futuro.add( cosa.submit( new numPerfectosPar((principio+(dif/cores)*i),finaleo)));
+      for (int j = 0; j < futuro.size(); j++) {
+        result += futuro.get(j).get();
+      }
+      System.out.println(result);
+      cosa.shutdown();
+
+    }
+    
+}
